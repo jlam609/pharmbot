fbca2dc85e4f934e97d48a37b98dc35f34b0ecee
jlam609 <jlam609@gmail.com>
2021-04-09 12:02:50 -0400

    changed up background and issues

--- src/Components/home.tsx
+++ src/Components/home.tsx
@@ -1,13 +1,45 @@
+import { Typography, Button } from "@material-ui/core";
 import React from "react";
+import { useHistory } from "react-router";
 
 const Home = () => {
+  const history = useHistory();
   return (
-    <div>
-      <img
-        src="https://i.ibb.co/4sjp1Bc/giant-panda-bei-bei-20180608-051skipbrown-wide-5621060ac353db3650a31d9b8cb078bbbfccd9e5.jpg"
-        alt="giant-panda-bei-bei"
-        className="panda"
-      />
+    <div className="home-background">
+      <div className="home">
+        <Typography variant="h2">Get a Free Consultation</Typography>
+        <img
+          src="https://miro.medium.com/max/1260/0*TMomIf3np7FUDufH.png"
+          height={200}
+          width={300}
+        />
+        <Button
+          variant="outlined"
+          fullWidth={false}
+          onClick={(e) => {
+            e.preventDefault();
+            history.push("/recommend");
+          }}
+        >
+          Click Here to Find Product Recommendations
+        </Button>
+        <br />
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjfeuvq8dY07lnrwmL6Bma50N_GUtCiCvedg&usqp=CAU"
+          height={200}
+          width={200}
+        />
+        <Button
+          variant="outlined"
+          fullWidth={false}
+          onClick={(e) => {
+            e.preventDefault();
+            history.push("/search");
+          }}
+        >
+          Click Here to Search For a Drug
+        </Button>
+      </div>
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,50 @@
-const path = require("path");
-const dotenv = require("dotenv");
+const path = require('path');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 module.exports = {
-  entry: path.join(__dirname, "/src/index.tsx"),
-  devtool: "source-map",
-  mode: "development",
+  entry: path.join(__dirname, '/src/index.tsx'),
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, './dist'),
+  },
+  devtool: 'source-map',
+  mode: 'development',
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
   module: {
     rules: [
       {
         test: /\.(t|j)sx?$/,
-        use: { loader: "ts-loader" },
+        use: { loader: 'ts-loader' },
         exclude: /node_modules/,
       },
-
-      // addition - add source-map support
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "source-map-loader",
+        loader: 'source-map-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
       },
     ],
   },
